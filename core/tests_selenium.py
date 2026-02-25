8cf2158132e6c2778c3d26a66be2b52eef04d5a6
Pol Cardona <polcar2005@gmail.com>
2026-02-25 17:40:27 +0100

    Modificar ruta i afegir verificació

--- core/tests_selenium.py
+++ core/tests_selenium.py
@@ -15,10 +15,8 @@ class SecurityRegressionTests(StaticLiveServerTestCase):
         options = Options()
         options.add_argument('--headless')
         
-        firefox_bin = os.environ.get('FIREFOX_BINARY')
-        if firefox_bin:
-            options.binary_location = firefox_bin
-
+        options.binary_location = '/usr/bin/firefox'
+        
         cls.selenium = WebDriver(options=options)
         cls.selenium.implicitly_wait(10)
 
